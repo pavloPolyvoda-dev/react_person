3f5da37298af27f7128f9ff22d4aed76aae361ae
Pavlo Polyvoda <pvlpeapod@gmail.com>
2026-03-26 09:58:00 +0200

    simplified code of Person.jsx

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,21 +1,16 @@
 export const Person = ({ person }) => {
-  let partnerStatus = '';
-
-  if (person.sex === 'm') {
-    partnerStatus = 'wife';
-  } else {
-    partnerStatus = 'husband';
-  }
+  const { name, age, sex, isMarried, partnerName } = person;
+  const partnerStatus = sex === 'm' ? 'wife' : 'husband';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {person.age && <p className="Person__age">I am {person.age}</p>}
+      {person.age && <p className="Person__age">I am {age}</p>}
 
-      {person.isMarried ? (
+      {isMarried ? (
         <p className="Person__partner">
-          {person.partnerName} is my {partnerStatus}
+          {partnerName} is my {partnerStatus}
         </p>
       ) : (
         <p className="Person__partner">I am not married</p>
